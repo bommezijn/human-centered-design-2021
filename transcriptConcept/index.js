ab3ffb48e924e78e3768f07dd08d57491e677690
dewarian <nathanbommezijn@gmail.com>
2021-04-21 12:48:57 +0200

    :poop: New test, testing on human reading

---
+++ transcriptConcept/index.js
@@ -0,0 +1,19 @@
+document.getElementById("fontSize").oninput = () => {
+  const fsSize = document.getElementById("fontSize").value;
+  document.querySelectorAll("main ul li").forEach((e) => {
+    e.style.fontSize = fsSize + "px";
+  });
+  document.getElementById("fontSizeShow").textContent = fsSize;
+};
+
+document.getElementById("lineHeight").oninput = () => {
+  const lhSize = document.getElementById("lineHeight").value;
+  document.querySelectorAll("main ul li").forEach((e) => {
+    e.style.lineHeight = lhSize + "px";
+  });
+  document.getElementById("lineHeightShow").textContent = lhSize;
+};
+
+// document.getElementById("visible").addEventListener("click", () => {
+//   document.querySelectorAll("section ul li").style.opacity = "0.5";
+// });
