8c179d441ce2b71b62fe18194ab821d3bdf55cfb
dewarian <nathanbommezijn@gmail.com>
2021-04-29 06:48:57 +0200

    :see_no_evil: Add another alert

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 console.log(`Hello Darice!`)
 
 let test = document.getElementById('notify');
+let text = document.getElementById('showText');
 let hide = document.getElementsByClassName('alert')[0];
+let otherMsg = document.getElementsByClassName('alert')[1];
 
 test.addEventListener('click', () => {
-  console.log('ey');
   hide.classList.toggle('hidden');
   setTimeout(hideClass, 4000)
 })
 
+text.addEventListener('click', () => {
+  console.log(otherMsg)
+  otherMsg.classList.toggle('hidden');
+  console.log('test')
+  setTimeout(dirtyClass, 4000);
+})
+
+
 function hideClass() {
     hide.classList.toggle('hidden');
+}
+
+function dirtyClass() {
+  otherMsg.classList.toggle('hidden');
 }
